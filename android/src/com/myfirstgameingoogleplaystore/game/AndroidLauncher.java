e5b9132a8f6f680e6305f79966a21096c4dd2d4c
Susavan <susavanpal07@gmail.com>
2023-01-16 22:22:30 +0530

    Add files

--- android/src/com/myfirstgameingoogleplaystore/game/AndroidLauncher.java
+++ android/src/com/myfirstgameingoogleplaystore/game/AndroidLauncher.java
@@ -1,10 +1,9 @@
-package com.myfirst.game;
+package com.myfirstgameingoogleplaystore.game;
 
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.myfirst.game.CoinMan;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
